f8dbab2787844b9a9580c9aabe0e7f134bff6e13
Bhavesh suthar <49830410+bhavesh-suthar01@users.noreply.github.com>
2020-09-04 16:31:04 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+$(".menubtn").click(function(){
+    $(".mobile").toggleClass("mobile_view");
+    $(".profile").toggleClass("blur");
+    $(".li").toggleClass("lie");
+});
+
+$(".li").click(function(){
+    $(".mobile").toggleClass("mobile_view");
+    $(".profile").toggleClass("blur");
+    $(".li").toggleClass("lie");
+});
+
+$("a[href^='#']").click(function(e) {
+   e.preventDefault();
+   
+   var position = $($(this).attr("href")).offset().top;
+
+   $("body, html").animate({
+       scrollTop: position
+   }, 1000);
+});
+
+
+function loading() {
+    $(".loading").css("display" ,"none");
+}
